473ea5bb2f75450739deee4daf875bd4cd81cdb1
Joshua J Piluden <joshuajpiluden@Gmail.com>
2022-09-28 03:33:36 +0800

    Migrated local database into  a container.

---
+++ MyDemoProjects.Application/Infastructure/Data/Migrations/20220927192815_migratetonewsqlcontainer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyDemoProjects.Application.Migrations
+{
+    public partial class migratetonewsqlcontainer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
