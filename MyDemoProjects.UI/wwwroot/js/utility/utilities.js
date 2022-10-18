86d90f3020f1f08e33c95f1e89daefbf6f58f767
Joshua J Piluden <joshuajpiluden@Gmail.com>
2022-10-18 18:48:41 +0800

    Added audio message notification .

--- MyDemoProjects.UI/wwwroot/js/utility/utilities.js
+++ MyDemoProjects.UI/wwwroot/js/utility/utilities.js
@@ -4,3 +4,15 @@
         element.scrollTop = element.scrollHeight - element.clientHeight;
     }
 }
+
+window.PlayAudioFile = (src) => {
+    var audio = document.getElementById('audio-player');
+    if (audio != null) {
+        var audioSource = document.getElementById('playerSource');
+        if (audioSource != null) {
+            audioSource.src = src;
+            audio.load();
+            audio.play();
+        }
+    }
+}
