db938bd80268873917de92112b0f8ab1d67c8557
Joshua J Piluden <joshuajpiluden@Gmail.com>
2022-10-21 11:34:15 +0800

    Revert "Proper instanstiation of httpclient."
    This reverts commit c075810d2d170f009b2af223389956779a83e90b.

--- MyDemoProjects.AzureFunctions/RandomGotQuotes.cs
+++ MyDemoProjects.AzureFunctions/RandomGotQuotes.cs
@@ -9,59 +9,69 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Collections.Generic;
 using System.Net;
-using static Duende.IdentityServer.IdentityServerConstants;
-using System.Net.Mail;
-using System.Net.Sockets;
 
 namespace MyDemoProjects.AzureFunctions
 {
     public static class RandomGotQuotesHttpTrigger
     {
-        private static HttpClient httpClient = new HttpClient();
-
         [FunctionName("RandomGotQuotesHttpTrigger")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-            var gotQuuotesResponse = await httpClient.GetStreamAsync("https://api.gameofthronesquotes.xyz/v1/random");
-            if (gotQuuotesResponse == null)
+
+            using(var client = new HttpClient())
             {
-                return new NotFoundResult();
+                var gotQuuotesResponse = await client.GetStreamAsync("https://api.gameofthronesquotes.xyz/v1/random");
+                if(gotQuuotesResponse == null)
+                {
+                    return new NotFoundResult();
+                }
+                var gotQuuotesResult = JsonSerializer.Deserialize<RandomGOTQuotesResponse>(gotQuuotesResponse);
+
+                var embedRequest = new Embed("Random GOT Quotes", gotQuuotesResult.Sentence, 5814783, new Footer(gotQuuotesResult.Character.Name));
+                var rootRequest = new Root("",new List<Embed>{ embedRequest });
+                var serializedRequest = JsonSerializer.Serialize<Root>(rootRequest);
+                var postResponse = await client.PostAsync("https://discord.com/api/webhooks/1027409178727297024/R1ek93N_HYYdxotCdefbyQRGUiDW3KrRpgHvdkr04u6DhY8YR90v2Guq-i1nFMuoqswd", new StringContent(serializedRequest, System.Text.Encoding.UTF8, "application/json"));
+                if (!postResponse.IsSuccessStatusCode)
+                {
+                    return new BadRequestResult();
+                }
+                
+                log.LogInformation(postResponse.StatusCode.ToString()); 
+                return new OkObjectResult(rootRequest);
             }
-            var gotQuuotesResult = JsonSerializer.Deserialize<RandomGOTQuotesResponse>(gotQuuotesResponse);
-
-            var embedRequest = new Embed("Random GOT Quotes", gotQuuotesResult.Sentence, 5814783, new Footer(gotQuuotesResult.Character.Name));
-            var rootRequest = new Root("", new List<Embed> { embedRequest });
-            var serializedRequest = JsonSerializer.Serialize<Root>(rootRequest);
-            var postResponse = await httpClient.PostAsync("https://discord.com/api/webhooks/1027409178727297024/R1ek93N_HYYdxotCdefbyQRGUiDW3KrRpgHvdkr04u6DhY8YR90v2Guq-i1nFMuoqswd", new StringContent(serializedRequest, System.Text.Encoding.UTF8, "application/json"));
-            if (!postResponse.IsSuccessStatusCode)
-            {
-                return new BadRequestResult();
-            }
-
-            log.LogInformation(postResponse.StatusCode.ToString());
-            return new OkObjectResult(rootRequest);
-
         }
 
-        //[FunctionName("RandomGotQuotesTimeTrigger")]
-        //public static async Task Run([TimerTrigger("0 0 */6 * * *")] TimerInfo myTimer, ILogger log)
-        //{
-        //    log.LogInformation("Function triggered.");
+    }
 
-        //    var gotQuuotesResponse = await httpClient.GetStreamAsync("https://api.gameofthronesquotes.xyz/v1/random");
-        //    var gotQuuotesResult = JsonSerializer.Deserialize<RandomGOTQuotesResponse>(gotQuuotesResponse);
+    public static class RandomGotQuotesTimeTrigger
+    {
+        /// <summary>
+        /// Runs every 6 hrs.
+        /// </summary>
+        /// <param name="myTimer"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        [FunctionName("RandomGotQuotesTimeTrigger")]
+        public static async Task Run([TimerTrigger("0 0 */6 * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation("Function triggered.");
 
-        //    var embedRequest = new Embed("Random GOT Quotes", gotQuuotesResult.Sentence, 5814783, new Footer(gotQuuotesResult.Character.Name));
-        //    var rootRequest = new Root("", new List<Embed> { embedRequest });
-        //    var serializedRequest = JsonSerializer.Serialize<Root>(rootRequest);
-        //    var postResponse = await httpClient.PostAsync("https://discord.com/api/webhooks/1027409178727297024/R1ek93N_HYYdxotCdefbyQRGUiDW3KrRpgHvdkr04u6DhY8YR90v2Guq-i1nFMuoqswd", new StringContent(serializedRequest, System.Text.Encoding.UTF8, "application/json"));
+            using (var client = new HttpClient())
+            {
+                var gotQuuotesResponse = await client.GetStreamAsync("https://api.gameofthronesquotes.xyz/v1/random");
+                var gotQuuotesResult = JsonSerializer.Deserialize<RandomGOTQuotesResponse>(gotQuuotesResponse);
 
-        //    log.LogInformation(postResponse.StatusCode.ToString());
+                var embedRequest = new Embed("Random GOT Quotes", gotQuuotesResult.Sentence, 5814783, new Footer(gotQuuotesResult.Character.Name));
+                var rootRequest = new Root("", new List<Embed> { embedRequest });
+                var serializedRequest = JsonSerializer.Serialize<Root>(rootRequest);
+                var postResponse = await client.PostAsync("https://discord.com/api/webhooks/1027409178727297024/R1ek93N_HYYdxotCdefbyQRGUiDW3KrRpgHvdkr04u6DhY8YR90v2Guq-i1nFMuoqswd", new StringContent(serializedRequest, System.Text.Encoding.UTF8, "application/json"));
 
-        //}
+                log.LogInformation(postResponse.StatusCode.ToString());
+            }
+        }
     }
 
     public record Character(
